d35d6428910fd7e69bfeac98d919cfeee12c8b55
TiUnidentis <ti@unidentis.com.br>
2021-06-01 11:05:43 -0300

    initialize new project in Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
   title = 'bytebank';
+  transferencias: any[] = [];
+
+  transferir($event: any) {
+    console.log($event);
+
+    const transferencia = {...$event, data: new Date()};
+    this.transferencias.push(transferencia);
+  }
 }
---
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,30 @@
+import { Component, EventEmitter, Output } from "@angular/core";
+
+@Component({
+  selector:'app-nova-transferencia',
+  templateUrl: './nova-transferencia.component.html',
+  styleUrls:['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent{
+
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number | undefined;
+  destino: number | undefined;
+
+  transferir(){
+    console.log('Solicitar nova transferencia');
+
+    const valorEmitir = {valor: this.valor, destino: this.destino}
+    this.aoTransferir.emit(valorEmitir);
+
+    this.limparCampos();
+  }
+
+  limparCampos() {
+    this.valor = 0;
+    this.destino = 0;
+  }
+
+}
